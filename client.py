c8d830b5b5791d0e0c5e7eb2f58a5819faca2c13
SohamShrivastava <sohamshrivastava0935@gmail.com>
2025-09-14 18:24:26 +0530

    Update file

---
+++ client.py
@@ -0,0 +1,60 @@
+from scapy.all import rdpcap, UDP, DNS
+from datetime import datetime
+import socket
+import pandas as pd
+
+pcap_file = "/Users/sohamshrivastava/Library/CloudStorage/OneDrive-iitgn.ac.in/Semester 5/CN/0.pcap"
+server_ip = "127.0.0.1"
+server_port = 12345
+
+def send_to_server(payload):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((server_ip, server_port))
+            s.sendall(payload)
+            data = s.recv(1024)
+            return data.decode('utf-8', errors='ignore')
+        except ConnectionRefusedError:
+            print("connection to server failed.")
+            return None
+
+def main():
+    try:
+        packets = rdpcap(pcap_file)
+    except FileNotFoundError:
+        print(f"pcap file '{pcap_file}' not found.")
+        return
+
+    dns_query_id = 0
+    results_data = []
+
+    print("Starting to process packets: \n")
+    print()
+
+    for packet in packets:
+        if packet.haslayer(DNS) and packet[DNS].qr == 0 and packet.haslayer(UDP) and packet[UDP].dport == 53:
+            now = datetime.now()
+            hh = now.strftime("%H")
+            mm = now.strftime("%M")
+            ss = now.strftime("%S")
+
+            id_str = f"{dns_query_id:02d}"
+            custom_header = f"{hh}{mm}{ss}{id_str}"
+
+            header_bytes = custom_header.encode('utf-8')
+            orgnl_dns_bytes = bytes(packet[DNS])
+            payload = header_bytes + orgnl_dns_bytes
+
+            resolved_ip = send_to_server(payload)
+            if resolved_ip:
+                domain_name = packet[DNS].qd.qname.decode('utf-8', errors='ignore')
+                print(f"Query ID: {dns_query_id}, Domain: {domain_name}, Resolved IP: {resolved_ip}")
+                results_data.append({"Custom Header value (HHMMSSID)": custom_header,"Domain name": domain_name,"Resolved IP address": resolved_ip})
+            
+            dns_query_id += 1
+    report_df = pd.DataFrame(results_data)
+    print("\nSummary of all queries:")
+    print(report_df.to_string())
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,76 @@
+import socket
+import json
+from scapy.all import DNS
+
+
+host = '127.0.0.1'
+port = 12345
+
+
+#15 IPs for maintaing load balancing
+ip_pool = ["192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5",
+"192.168.1.6", "192.168.1.7", "192.168.1.8", "192.168.1.9", "192.168.1.10", 
+"192.168.1.11", "192.168.1.12", "192.168.1.13", "192.168.1.14", "192.168.1.15"
+]
+
+# header format: HHMMSSID
+def get_resolved_ip(header):
+    try:
+        hour = int(header[0:2])
+        query_id = int(header[6:8])
+
+    except(ValueError, IndexError):
+        print("Invalid Header")
+
+    #determining ip pool through hour
+    ip_pool_start = 0
+    if hour >= 4 and hour <= 11:
+        ip_pool_start = 0
+    elif hour >= 12 and hour <= 19:
+        ip_pool_start = 5
+    else:
+        ip_pool_start = 10
+
+    #select ip from pool
+    offset = query_id % 5
+    final_idx = ip_pool_start + offset
+
+
+    #boundary check for index
+    if final_idx >= 0 and final_idx < len(ip_pool):
+        return ip_pool[final_idx]
+    else:
+        print("Index out of range")    
+
+def server():
+    # create a tcp/ip socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f"server listening on {host}:{port}")
+
+        while True: #wait for connection
+            conn, addr = s.accept()
+            with conn:
+                print(f"connected by {addr}")
+                data = conn.recv(1024) # receive data from client 
+                if not data:
+                    break
+                
+                header_bytes = data[:8] #first 8 bytes of header
+                dns_packet_bytes = data[8:] #rest is dns query
+
+                custom_header = header_bytes.decode('utf-8', errors='ignore')
+
+                #parsing dns query 
+                dns_query = DNS(dns_packet_bytes)
+                domain_name = dns_query.qd.qname.decode('utf-8', errors='ignore')
+                resolved_ip = get_resolved_ip(custom_header) #getting resolved ip
+
+                print(f"received query for: {domain_name}, with header: {custom_header}")
+                print(f"resolved ip: {resolved_ip}")
+                conn.sendall(resolved_ip.encode('utf-8')) #send resolved IP back to client
+                print("response sent back to client\n")
+
+if __name__ == "__main__":
+    server()
